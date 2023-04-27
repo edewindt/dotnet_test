bad6bde71154df92fbdceecb76a433398898f28f
Elias De Windt <edewindt27@outlook.com>
2023-04-27 00:22:10 -0700

    made first middleware

--- Program.cs
+++ Program.cs
@@ -6,21 +6,31 @@ var app = builder.Build();
 // app.MapGet("/anime", () => "Anime Endpoint");
 
 
+// app.Run(async (HttpContext context) => 
+// {
+//     string userAgent;
+//     // string path = context.Request.Path;
+//     // string method = context.Request.Method;
+//     if (context.Request.Headers.ContainsKey("User-Agent"))
+//     {
+//         userAgent = context.Request.Headers["User-Agent"];
+
+//         context.Response.Headers["Content-Type"] = "text/html";
+//     await context.Response.WriteAsync($"<h1>Hello Bro! {userAgent} </h1>");
+//     }
+    
+    
+// }
+// );
+
+app.Use(async (HttpContext context, RequestDelegate next) => 
+{
+    await context.Response.WriteAsync("First");
+    await next(context);
+});
 app.Run(async (HttpContext context) => 
 {
-    string userAgent;
-    // string path = context.Request.Path;
-    // string method = context.Request.Method;
-    if (context.Request.Headers.ContainsKey("User-Agent"))
-    {
-        userAgent = context.Request.Headers["User-Agent"];
-
-        context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync($"<h1>Hello Bro! {userAgent} </h1>");
-    }
-    
-    
-}
-);
+    await context.Response.WriteAsync("Second");
+});
 
 app.Run();
