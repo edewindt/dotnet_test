54d4d17029636a67f91ac003dbd31d079dd9f2ef
Elias De Windt <edewindt27@outlook.com>
2023-04-29 15:26:39 -0700

    added UseWhen middleware

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-using MiddlewareExample.MyFirstMiddleware;
+// using MiddlewareExample.MyFirstMiddleware;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<MyFirstMiddleware>();
+// builder.Services.AddTransient<MyFirstMiddleware>();
 var app = builder.Build();
 
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("name"),
+    app => {
+        app.Use(async (context, next) => {
+            await context.Response.WriteAsync("Hello from UseWhen ");
+            await next();
+        });
+    }
+);
+app.Run(async context => {
+    await context.Response.WriteAsync("Hello from regular middleware");
+});
 // app.MapGet("/", () => "Hello World!");
 
 // app.MapGet("/anime", () => "Anime Endpoint");
@@ -21,26 +33,27 @@ var app = builder.Build();
 //         context.Response.Headers["Content-Type"] = "text/html";
 //     await context.Response.WriteAsync($"<h1>Hello Bro! {userAgent} </h1>");
 //     }
-    
-    
+
+
 // }
 // );
 
-app.Use(async (HttpContext context, RequestDelegate next) => 
-{
-    await context.Response.WriteAsync("First ");
-    await next(context);
-});
-app.UseMyMiddleware();
+// app.Use(async (HttpContext context, RequestDelegate next) => 
+// {
+//     await context.Response.WriteAsync("First ");
+//     await next(context);
+// });
+// app.UseMyMiddleware();
 
-app.Use(async (HttpContext context, RequestDelegate next) => 
-{
-    await context.Response.WriteAsync("Second ");
-    await next(context);
-});
-app.Run(async (HttpContext context) => 
-{
-    await context.Response.WriteAsync("Third ");
-});
+// app.Use(async (HttpContext context, RequestDelegate next) => 
+// {
+//     await context.Response.WriteAsync("Second ");
+//     await next(context);
+// });
+// app.Run(async (HttpContext context) => 
+// {
+//     await context.Response.WriteAsync("Third ");
+// });
+
 
 app.Run();
