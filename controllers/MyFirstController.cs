ef2698fbb19f3cfd5e30fbf347a9fdb9ec01c787
Elias De Windt <edewindt27@outlook.com>
2023-05-01 02:32:13 -0700

    added many routes

--- controllers/MyFirstController.cs
+++ controllers/MyFirstController.cs
@@ -4,10 +4,26 @@ namespace ControllersExample.Controllers
 {
    public class MyFirstController : Controller
     {
-        [Route("hello")]
-        public string method1()
+        [Route("/")]
+        public string Index()
         {
-            return "Hello From The Otherside";
+            return "Hello World!";
+        }
+        [Route("/anime")]
+        public string Anime()
+        {
+            return "Anime Endpoint";
+        }
+        [Route("/test")]
+        public string Test()
+        {
+            return "Test Endpoint";
+        }
+        [Route("/docs/{id}")]
+        public string Documents()
+        {
+            int id = Convert.ToInt32(Request.RouteValues["id"]);
+            return $"Document {id}";
         }
     }
     
